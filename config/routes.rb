90f2f3c2a2a5d9ae16c8a64f4334260ba1b70d63
Mena Castillo <100647073+Meenoow@users.noreply.github.com>
2023-11-16 17:53:34 +0000

    sign in and sign up forms are working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -6,5 +7,6 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "home#index"
   resources :notes
+  resources :dashboard
 
 end
