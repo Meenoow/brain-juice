0227b92f052b4cc8036a4daf56b9e1bfc5f60cfa
Mena Castillo <100647073+Meenoow@users.noreply.github.com>
2023-11-28 17:42:00 +0000

    Design change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,8 @@ class ApplicationController < ActionController::Base
   def after_sign_up_path_for(resource)
     dashboard_path
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   #get 'dashboard/index'
   get 'dashboard', to: 'dashboard#index', as: :dashboard
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout' }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -9,5 +10,4 @@ Rails.application.routes.draw do
   root "home#index"
   resources :notes
   resources :dashboard
-
 end
